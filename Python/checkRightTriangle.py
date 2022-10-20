418d602869c2fc578f8aa8e31a539b44f3bd9255
Mohammed Ahmed <46112529+techsupportmo@users.noreply.github.com>
2022-10-20 11:41:14 -0500

    adding another way to solve problem

--- Python/checkRightTriangle.py
+++ Python/checkRightTriangle.py
@@ -7,6 +7,25 @@ def checkRightTriangle(a, b, c):
     else:
         return False
 
+def anotherMethod(a, b, c):
+
+    # First store values into an arrary
+    sides = []
+    sides.append(a)
+    sides.append(b)
+    sides.append(c)
+
+    # Sort the array in ascending order
+    sides.sort()
+
+    # Check pythagorean theorem
+    if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2]):
+        return True
+    else:
+        return False
 
 print(checkRightTriangle(10,10,10))
 print(checkRightTriangle(3,4,5))
+print("")
+print(anotherMethod(12,2,1))
+print(anotherMethod(5,4,3))
